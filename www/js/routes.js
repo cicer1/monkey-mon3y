736b90b7966953f442719cb7980fcd2b50b50466
Paulo Sa <ph@phprint.com.br>
2017-07-07 13:13:04 +0200

    Initial commit

---
+++ www/js/routes.js
@@ -0,0 +1,45 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('splash', {
+    url: '/splash',
+    templateUrl: 'templates/splash.html',
+    controller: 'splashCtrl'
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('accounts', {
+    url: '/accounts',
+    templateUrl: 'templates/accounts.html',
+    controller: 'accountsCtrl'
+  })
+
+  .state('transaction', {
+    url: '/transaction',
+    templateUrl: 'templates/transaction.html',
+    controller: 'transactionCtrl'
+  })
+
+  .state('settings', {
+    url: '/settings',
+    templateUrl: 'templates/settings.html',
+    controller: 'settingsCtrl'
+  })
+
+$urlRouterProvider.otherwise('/splash')
+
+
+});
